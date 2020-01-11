ca09084ec73819c97d8897af61c376bc55984dd6
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-11 10:02:01 -0700

    prompt

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+//prompting
+
+var inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ])
+
+
+
+
+
+
 const colors = {
   green: {
     wrapperBackground: "#E6E1C3",
